ed9b556b9c78bc2498bfd81ce004faa9f87c5b7f
Henry Wilder <henrythepony@gmail.com>
2023-10-22 00:02:14 -0400

    Added acrylic brush

--- AlgebraBalancer/App.xaml.cs
+++ AlgebraBalancer/App.xaml.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -30,6 +33,14 @@ namespace AlgebraBalancer
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+        }
+
+        private void ExtendAcrylicIntoTitleBar()
+        {
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
         }
 
         /// <summary>
@@ -70,6 +81,8 @@ namespace AlgebraBalancer
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
+
+                ExtendAcrylicIntoTitleBar();
             }
         }
 
