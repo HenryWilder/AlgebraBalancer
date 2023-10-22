2de32e44734720b3e35b070ff83745b97f561552
Henry Wilder <henrythepony@gmail.com>
2023-10-22 00:47:33 -0400

    Fixed case in which smaller number is a factor of the larger number

--- AlgebraBalancer/MainPage.xaml.cs
+++ AlgebraBalancer/MainPage.xaml.cs
@@ -166,7 +166,7 @@ namespace AlgebraBalancer
                 return factors;
             }
 
-            for (int i = 1; i < Math.Min(a, b); ++i)
+            for (int i = 1; i <= Math.Min(a, b); ++i)
             {
                 if (a % i != 0 || b % i != 0) { continue; }
                 factors.Add((i, a / i, b / i));
