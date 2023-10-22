ed0a478da7f6a53d8b06fcd697cdf6b2f30da399
Henry Wilder <henrythepony@gmail.com>
2023-10-21 21:23:20 -0400

    Added binary space

--- AlgebraBalancer/MainPage.xaml.cs
+++ AlgebraBalancer/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using static System.Net.Mime.MediaTypeNames;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace AlgebraBalancer
@@ -39,52 +41,72 @@ namespace AlgebraBalancer
             this.InitializeComponent();
         }
 
-        private void Factor(object sender, TextChangedEventArgs args)
+        private void Update(object sender, TextChangedEventArgs args)
         {
-            string input = InputBox.Text;
             try
             {
-                int n = int.Parse(input);
-                var factors = new List<FactorPair>
-                {
-                    new FactorPair(1, n)
-                };
+                string aText = InputA.Text;
+                string bText = InputB.Text;
 
-                if (n > 0) // Positive
+                if (aText.Length > 10 || bText.Length > 10)
                 {
-                    for (int i = 2; i < (n / i); ++i)
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (aText == string.Empty || bText == string.Empty) // Unary
+                {
+                    string text = aText == string.Empty ? bText : aText;
+                    int x = int.Parse(text);
+
+                    var factors = Factors(x);
+                    int factorsPad1 = 0;
+                    int factorsPad2 = 0;
+                    foreach (var f in factors)
                     {
-                        factors.Add(new FactorPair(i, n / i));
+                        factorsPad1 = Math.Max(f.a.ToString().Length, factorsPad1);
+                        factorsPad2 = Math.Max(f.b.ToString().Length, factorsPad2);
                     }
-                }
-                else if (n < 0) // Negative
-                {
-                    for (int i = 2; i < -(n / i); ++i)
-                    {
-                        factors.Add(new FactorPair(i, n / i));
-                    }
-                }
 
-                string leftText  = factors[0].a.ToString();
-                string timesText = "×";
-                string rightText = factors[0].b.ToString();
-                for (int i = 1; i < factors.Count(); ++i)
-                {
-                    leftText  += "\n" + factors[i].a.ToString();
-                    timesText += "\n×";
-                    rightText += "\n" + factors[i].b.ToString();
+                    OutputFactors.Text = string.Join("\n",
+                        from f in factors
+                        select f.a.ToString().PadLeft(factorsPad1) + " × " + f.b.ToString().PadLeft(factorsPad2));
                 }
-
-                ResultBoxLeft .Text = leftText;
-                ResultBoxTimes.Text = timesText;
-                ResultBoxRight.Text = rightText;
+                else // Binary
+                {
+                    int a = int.Parse(aText);
+                    int b = int.Parse(bText);
+                }
             }
-            catch (FormatException)
+            catch
             {
-                ResultBoxLeft .Text = "...";
-                ResultBoxTimes.Text = "×";
-                ResultBoxRight.Text = "...";
+                OutputFactors.Text = "...";
             }
+        }
+
+        private List<FactorPair> Factors(int n)
+        {
+            var factors = new List<FactorPair>
+            {
+                new FactorPair(1, n)
+            };
+
+            if (n > 0) // Positive
+            {
+                for (int i = 2; i < (n / i); ++i)
+                {
+                    if (n % i != 0) { continue; }
+                    factors.Add(new FactorPair(i, n / i));
+                }
+            }
+            else if (n < 0) // Negative
+            {
+                for (int i = 2; i < -(n / i); ++i)
+                {
+                    if (n % i != 0) { continue; }
+                    factors.Add(new FactorPair(i, n / i));
+                }
+            }
+            return factors;
         }
     }
 }
